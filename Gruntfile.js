1ce4823375f00a8ef0bea291404614b1e94b3482
Scott Rouse <scott.rouse@gmail.com>
2016-07-27 11:55:48 -0500

    adding all the models.

---
+++ Gruntfile.js
@@ -0,0 +1,139 @@
+
+
+module.exports = function(grunt) {
+
+    grunt.loadNpmTasks('cssmodeling');
+
+    var configObj = {
+        pkg: '<json:package.json>'
+    };
+
+    /*==========================
+    CSSMODELING
+    ==========================*/
+    configObj.cssmodeling = configObj.cssmodeling || {};
+
+
+    //=============SIMPLE===============
+    configObj.cssmodeling["cssmodeling_simple_less"] = {
+        files: {
+            'dist/simple/cssmodeling_simple':
+            ['models/simple/cssmodeling_simple.json']
+        },
+        options: {
+            type:"less",
+            var_prefix:"v-"
+        }
+    };
+
+    configObj.cssmodeling["cssmodeling_simple_scss"] = {
+        files: {
+            'dist/simple/cssmodeling_simple':
+            ['models/simple/cssmodeling_simple.json']
+        },
+        options: {
+            type:"scss",
+            var_prefix:"v-"
+        }
+    };
+
+    //=============ROWS===============
+    configObj.cssmodeling["cssmodeling_rows_quartered_less"] = {
+        files: {
+            'dist/rows/cssmodeling_rows_quartered':
+            ['models/rows/cssmodeling_rows_quartered.json']
+        },
+        options: {
+            type:"less",
+            var_prefix:"v-"
+        }
+    };
+
+    configObj.cssmodeling["cssmodeling_rows_quartered_scss"] = {
+        files: {
+            'dist/rows/cssmodeling_rows_quartered':
+            ['models/rows/cssmodeling_rows_quartered.json']
+        },
+        options: {
+            type:"scss",
+            var_prefix:"v-"
+        }
+    };
+
+    //=============COLS===============
+    configObj.cssmodeling["cssmodeling_col_12_quartered_viewport_breakpoints_less"] = {
+        files: {
+            'dist/cols/cssmodeling_col_12_quartered_viewport_breakpoints':
+            ['models/cols/cssmodeling_col_12_quartered_viewport_breakpoints.json']
+        },
+        options: {
+            type:"less",
+            var_prefix:"v-"
+        }
+    };
+
+    configObj.cssmodeling["cssmodeling_col_12_quartered_viewport_breakpoints_scss"] = {
+        files: {
+            'dist/cols/cssmodeling_col_12_quartered_viewport_breakpoints':
+            ['models/cols/cssmodeling_col_12_quartered_viewport_breakpoints.json']
+        },
+        options: {
+            type:"scss",
+            var_prefix:"v-"
+        }
+    };
+
+    configObj.cssmodeling["cssmodeling_col_12_quartered_viewport_less"] = {
+        files: {
+            'dist/cols/cssmodeling_col_12_quartered_viewport':
+            ['models/cols/cssmodeling_col_12_quartered_viewport.json']
+        },
+        options: {
+            type:"less",
+            var_prefix:"v-"
+        }
+    };
+
+    configObj.cssmodeling["cssmodeling_col_12_quartered_viewport_scss"] = {
+        files: {
+            'dist/cols/cssmodeling_col_12_quartered_viewport':
+            ['models/cols/cssmodeling_col_12_quartered_viewport.json']
+        },
+        options: {
+            type:"scss",
+            var_prefix:"v-"
+        }
+    };
+
+
+    //=============FLEX===============
+    configObj.cssmodeling["cssmodeling_flex_less"] = {
+        files: {
+            'dist/flex/cssmodeling_flex':
+            ['models/flex/cssmodeling_flex.json']
+        },
+        options: {
+            type:"less",
+            var_prefix:"v-"
+        }
+    };
+
+    configObj.cssmodeling["cssmodeling_flex_scss"] = {
+        files: {
+            'dist/flex/cssmodeling_flex':
+            ['models/flex/cssmodeling_flex.json']
+        },
+        options: {
+            type:"scss",
+            var_prefix:"v-"
+        }
+    };
+
+
+
+    grunt.initConfig( configObj );
+    grunt.registerTask( 'default' , [
+        'cssmodeling'
+    ] );
+
+}
